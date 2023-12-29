04f99a8c9070b17fc2f8175989963202993d7d74
manonzlamarguerite <m.girard42@hotmail.com>
2023-12-29 12:33:21 +0100

    feat : ajout du modèle Baseline

---
+++ BaseLine/Model_indep_POS/exec.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Ajoute des mots inconnues dans le fichier d'entrainement
+python3 conllu_add_unk.py fr_gsd-ud-train.conllu 10 > train.10.conllu
+# Calcule les probabilité des bigrammes de POS
+python3 conllu_bigrams.py train.10.conllu train.10.big
+echo "Début de l'entrainement"
+# Entraine le modèle de prédiction de POS
+python3 train.py train.10.conllu train.10.pt train.10.voc
+echo "Fin de l'entraintement"
+# Permet l'inférence sur le fichier de test
+python3 decode.py train.10.pt fr_gsd-ud-test.conllu train.10.voc #> train.10.auto.conllu
+echo "Evaluation du modèle"
+# Permet d'évaluer le modèle
+python3 conllu_eval.py fr_gsd-ud-test.conllu train.10.auto.conllu
+echo "OK5"
---
+++ BaseLine/Model_joint/conllu_bigrams.py
@@ -0,0 +1,19 @@
+import sys
+from UDTagSet import UDTagSet
+from bigrams import Bigrams
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage :", sys.argv[0], "conlluFile bigramFile")
+        sys.exit(1)
+
+    conlluFileName  = sys.argv[1]
+    bigramFileName = sys.argv[2]
+
+    tagSet = UDTagSet(conlluFileName)
+    bigrams = Bigrams()
+    bigrams.computeFromConlluFile(conlluFileName, tagSet)
+    bigrams.save(bigramFileName)
+
+if __name__ == '__main__':
+    main()
